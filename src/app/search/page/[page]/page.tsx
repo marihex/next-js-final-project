fa2a1276d55fc544a614b659b304759ef649495c
rieznyk131 <m.rieznyk@gmail.com>
2026-07-21 18:25:50 +0200

    final commit 2. add pagination pages to search resuults

---
+++ src/app/search/page/[page]/page.tsx
@@ -0,0 +1,36 @@
+import React, {FC} from 'react';
+import {redirect} from "next/navigation";
+import {search} from "@/src/services/api.services";
+import {MoviesListComponent} from "@/src/components/movie/MoviesListComponent/MoviesListComponent";
+import PaginationComponent from "@/src/components/pagination/PaginationComponent";
+import '../../../../components/search/search-page-style.css'
+
+type Props = {
+    searchParams: Promise<{query: string}>;
+    params: Promise<{page: string}>
+}
+
+
+const SearchPaginationPage: FC<Props>  = async ({searchParams, params}) => {
+    const {page} = await params;
+    const {query} = await searchParams;
+    const currentPage: number = Number(page);
+
+    {
+        currentPage === 1 && redirect(`/search?query=${query}`)
+    }
+
+    const data = await search(query, currentPage);
+    const movies = data.results;
+    const totalPages = data.total_pages;
+
+    return (
+        <section className='search__section'>
+            <h1 className='search__header'>Search Results for {query.toUpperCase()}</h1>
+            <MoviesListComponent movies={movies}/>
+            <PaginationComponent totalPages={totalPages} currentPage={currentPage} basePath={`/search`}/>
+        </section>
+    );
+};
+
+export default SearchPaginationPage;
